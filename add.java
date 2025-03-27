410c409fc8438084b390ff75168227a550ea0f5d
Punya254 <vvce22ise0073@vvce.ac.in>
2025-03-27 10:56:10 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,13 @@
+public class add {
+    public static void main(String[] args) {
+        // Define the numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
